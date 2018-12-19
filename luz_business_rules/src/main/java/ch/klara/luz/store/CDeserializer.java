cdfa45a42eaabb6e6333acc8a80f7c69311054f5
bqvinh <>
2018-12-19 11:30:32 +0700

    {/luz_business_rules/src/main/java/ch/klara/luz/store/CDeserializer.java}

--- luz_business_rules/src/main/java/ch/klara/luz/store/CDeserializer.java
+++ luz_business_rules/src/main/java/ch/klara/luz/store/CDeserializer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
@@ -13,9 +15,14 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 public class CDeserializer extends JsonDeserializer<LocalDate> {
 	
 	public LocalDate deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-		if (parser.getCurrentToken() == JsonToken.VALUE_NUMBER_INT) {
-			long t = parser.getLongValue();
-			return Instant.ofEpochMilli(t).atZone(ZoneId.systemDefault()).toLocalDate();
+		if (parser.getCurrentToken() == JsonToken.VALUE_STRING) {
+			String string = parser.getText().trim();
+			if (string.length() == 0) {
+				return null;
+			} else {
+				Instant instant = Instant.parse(string);
+				return LocalDateTime.ofInstant(instant, ZoneOffset.UTC).toLocalDate();
+			}
 		}
 		return null;
 	}
